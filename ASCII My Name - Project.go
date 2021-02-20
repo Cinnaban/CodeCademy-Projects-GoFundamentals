c8395c1bd95cd8a80967574a84c048444c053c6e
Charles <38198248+Cinnaban@users.noreply.github.com>
2021-02-19 23:52:14 -0800

    ASCII My Name - Project
    Used go to write my name in ASCII

---
+++ ASCII My Name - Project.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main () {
+  fmt.Println("Hello There")
+  fmt.Println("  .o88b. db   db  .d8b.  d8888b. db      d88888b .d8888. ")
+  fmt.Println("d8P  Y8 88   88 d8' `8b 88  `8D 88      88'     88'  YP ")
+  fmt.Println("8P      88ooo88 88ooo88 88oobY' 88      88ooooo `8bo.   ")
+  fmt.Println("8b      88~~~88 88~~~88 88`8b   88      88~~~~~   `Y8b. ")
+  fmt.Println("Y8b  d8 88   88 88   88 88 `88. 88booo. 88.     db   8D ")
+  fmt.Println("`Y88P' YP   YP YP   YP 88   YD Y88888P Y88888P `8888Y' ")
+                                                        
+  fmt.Println(" .d8b.  d8888b.  .d8b.  d8b   db ")
+  fmt.Println("d8' `8b 88  `8D d8' `8b 888o  88 ")
+  fmt.Println("88ooo88 88oooY' 88ooo88 88V8o 88 ")
+  fmt.Println("88~~~88 88~~~b. 88~~~88 88 V8o88 ")
+  fmt.Println("88   88 88   8D 88   88 88  V888 ")
+  fmt.Println("YP   YP Y8888P' YP   YP VP   V8P ")
+}
